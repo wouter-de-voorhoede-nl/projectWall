dacecf26fffaea044946eed9c69dd03333a90d0d
wouter <wouter@voorhoede.nl>
2016-07-22 15:47:21 +0200

    feat(socket.io): add websocket

--- app.js
+++ app.js
@@ -1,10 +1,40 @@
-var express = require('express');
-var app = express();
+var WebSocketServer = require("ws").Server
+var http = require("http")
+var express = require("express")
+var app = express()
+var port = process.env.PORT || 5000
 
-app.get('/', function (req, res) {
-  res.send('Hello World!!');
-});
+app.use(express.static(__dirname + "/"))
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
+var server = http.createServer(app)
+server.listen(port)
+
+console.log("http server listening on %d", port)
+
+var wss = new WebSocketServer({server: server})
+console.log("websocket server created")
+
+wss.broadcast = function(data) {
+  for (var i in this.clients)
+    this.clients[i].send(data);
+};
+
+
+wss.on("connection", function(ws) {
+
+  console.log("websocket connection open");
+
+  ws.on('message', function(message) {
+    	//wss.broadcast(message);
+    	console.log(message);
+    	wss.broadcast(message);
+    })
+
+  ws.on("close", function() {
+    console.log("websocket connection close")
+  })
+
+  ws.on('sending', function() {
+  	console.log('sending');
+  })
+})
